b98fe459455a4970f66fc4d3c24ef7e7df4cdaa8
vainikkk <vainik.kakadia@gmail.com>
2020-11-05 20:22:53 +0530

    find movie form id

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -3,27 +3,38 @@ var router = express.Router();
 let Movie = require("../models/movies")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log(req.query)
-  if(req.query){
-    if(req.query.genre){
-      Movie.find({genres : {$exists: true, $all: [req.query.genre]}, type: "movie"}).exec((err, movies) => {
-        if(err){
+  if (req.query) {
+    if (req.query.genre) {
+      Movie.find({ genres: { $exists: true, $all: [req.query.genre] }, type: "movie", released: { $gt: new Date("2012-01-01") }, "imdb.rating": { $gt: 8 } }).exec((err, movies) => {
+        if (err) {
           res.send('error has occured');
         }
-        else{
+        else {
+          let dummy = movies.slice(0, 50)
+          res.json(dummy);
+        }
+      })
+    }
+    else if (req.query.country) {
+      Movie.find({ countries: { $exists: true, $all: [req.query.country] }, type: "movie", released: { $gt: new Date("2012-01-01") }, "imdb.rating": { $gt: 8 } }).exec((err, movies) => {
+        if (err) {
+          res.send('error has occured');
+        }
+        else {
           let dummy = movies.slice(0, 50)
           res.json(dummy);
         }
       })
     }
   }
-  else{
-    Movie.find({released: {$gt: new Date("2016-01-01")}}).exec((err, movies) => {
-      if(err){
+  else {
+    Movie.find({ released: { $gt: new Date("2016-01-01") } }).exec((err, movies) => {
+      if (err) {
         res.send('error has occured');
       }
-      else{
+      else {
         let dummy = movies.slice(0, 20)
         res.json(dummy);
       }
@@ -31,16 +42,27 @@ router.get('/', function(req, res, next) {
   }
 });
 
-router.get('/topRated', function(req, res, next) {
-  Movie.find({"imdb.rating": {$gt: 9}}).exec((err, movies) => {
-    if(err){
+router.get('/topRated', function (req, res, next) {
+  Movie.find({ "imdb.rating": { $gt: 8.5 } }).exec((err, movies) => {
+    if (err) {
       res.send('error has occured');
     }
-    else{
-      let dummy = movies.slice(0, 20)
+    else {
+      let dummy = movies.slice(0, 17)
       res.json(dummy);
     }
   })
+});
+
+router.get('/:id', function (req, res, next) {
+  console.log(req.params)
+  Movie.findById(req.params.id, function(err, result) {
+    if (err) {
+      res.send('error has occured');
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 module.exports = router;
